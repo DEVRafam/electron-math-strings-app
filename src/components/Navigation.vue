6a7ec963843fc701a7dd7d0ab1343aef5715b2c8
DEVRafam <jodax95@gmail.com>
2021-02-11 03:06:27 +0100

    building prod hotfix

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="nav">
-        <!-- <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> -->
+        <router-link to="/" class="redirect-to-home">HOME</router-link>
     </div>
 </template>
